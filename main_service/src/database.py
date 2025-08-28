7b14a7ef3a4beb6bad5dd98c7a0d780244eee588
Nikuta <vapasira328@gmail.com>
2025-08-28 16:58:04 +0300

    add real db

--- main_service/src/database.py
+++ main_service/src/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"
+SQLALCHEMY_DATABASE_URL = "postgresql://Admin:Admin@postgres_auth:5432/auth_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
