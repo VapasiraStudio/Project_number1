404191d6023ed435d9795e691a203e31175f9f5f
Nikuta <vapasira328@gmail.com>
2025-08-28 16:28:18 +0300

    fix bags db

--- main_service/src/crud.py
+++ main_service/src/crud.py
@@ -1,11 +1,10 @@
 from sqlalchemy.orm import Session
-import chemas
 import models
-
+import schemas
 def get_lists(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.List).offset(skip).limit(limit).all()
 
-def create_list(db: Session, list: chemas.ListCreate):
+def create_list(db: Session, list: schemas.ListCreate):
     db_list = models.List(name=list.name)
     db.add(db_list)
     db.commit()
@@ -14,8 +13,8 @@ def create_list(db: Session, list: chemas.ListCreate):
 
 def get_links(db: Session, list_id: int | None = None, search: str | None = None):
     query = db.query(models.Link)
-    if list_id:
-        query = query.filter(models.Link.list_id == list_id)
+    if list_id is not None:
+        query = query.filter_by(list_id=list_id)
     if search:
         query = query.filter(
             (models.Link.title.ilike(f"%{search}%")) |
@@ -23,16 +22,15 @@ def get_links(db: Session, list_id: int | None = None, search: str | None = None
         )
     return query.all()
 
-
-def create_link(db: Session, link: chemas.LinkCreate):
+def create_link(db: Session, link: schemas.LinkCreate):
     db_link = models.Link(**link.dict())
     db.add(db_link)
     db.commit()
     db.refresh(db_link)
     return db_link
 
-def update_link(db: Session, link_id: int, link: chemas.LinkCreate):
-    db_link = db.query(models.Link).filter(models.Link.id == link_id).first()
+def update_link(db: Session, link_id: int, link: schemas.LinkCreate):
+    db_link = db.query(models.Link).filter_by(id=link_id).first()
     if db_link:
         for key, value in link.dict().items():
             setattr(db_link, key, value)
@@ -40,3 +38,9 @@ def update_link(db: Session, link_id: int, link: chemas.LinkCreate):
         db.refresh(db_link)
     return db_link
 
+def delete_link(db: Session, link_id: int):
+    db_link = db.query(models.Link).filter_by(id=link_id).first()
+    if db_link:
+        db.delete(db_link)
+        db.commit()
+    return db_link
--- main_service/src/database.py
+++ main_service/src/database.py
@@ -1,15 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-session = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 def get_db():
-    db = session()
+    db = SessionLocal()
     try:
         yield db
     finally:
